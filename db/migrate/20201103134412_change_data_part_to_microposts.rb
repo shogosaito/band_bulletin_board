f040c6dc64b7c06a4b24bcf7b783e2608f913363
S.Shogo <>
2021-03-21 18:25:13 +0900

    不要なファイル削除

--- db/migrate/20201103134412_change_data_part_to_microposts.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataPartToMicroposts < ActiveRecord::Migration[6.0]
-  def change
-    change_column :microposts, :part, :integer
-  end
-end
--- db/migrate/20201230004810_change_column.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumn < ActiveRecord::Migration[6.0]
-  def change
-    change_column :microposts, :part, :string
-  end
-end
