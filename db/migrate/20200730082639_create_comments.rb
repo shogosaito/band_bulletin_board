356fa53fd1786d2ec034d55a0a9f367179538788
S.Shogo <>
2021-03-21 17:59:28 +0900

    migrationファイル修正

--- db/migrate/20200730082639_create_comments.rb
+++ db/migrate/20200730082639_create_comments.rb
@@ -4,7 +4,6 @@ class CreateComments < ActiveRecord::Migration[6.0]
       t.string :content
       t.references :user, null: false, foreign_key: true
       t.references :micropost, null: false, foreign_key: true
-
       t.timestamps
     end
   end
