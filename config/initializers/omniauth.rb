d64d115c2019b863cfeff10b7a0d9f4df8f953d4
S.Shogo <>
2021-03-31 21:42:40 +0900

    scope変更

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-           scope: 'https://www.googleapis.com/auth/userinfo.email,
-              https://www.googleapis.com/auth/userinfo.profile,
-              https://www.googleapis.com/auth/calendar',
+          scope: 'email calendar',
           display: 'popup'
   { provider_ignores_state: true }
 end
