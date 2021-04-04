caab8f8dd2fbce1d8f228ceffd0974e01beedd59
S.Shogo <>
2021-04-05 00:19:17 +0900

    メール設定変更

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: 'xiangwuzhaiteng652@gmail.com',
-    password: 'rcefljtjanvazahi',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true,
   }
