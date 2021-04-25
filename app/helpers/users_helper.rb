ee8ad0384cddb01b76241aabb38dff28b2ba391e
S.Shogo <>
2021-04-26 00:34:20 +0900

    Remove: 不要メソッド削除

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,4 @@
 module UsersHelper
-  def image_for(user)
-    if user.image
-      image_tag "/user_images/#{user.image}"
-    else
-      image_tag "wanko.png"
-    end
-  end
-
   # 渡された年齢に本日なったばかりの生年月日をyyyymmdd形式で出力
   def calc_min_birthday(age)
     Date.today.strftime("%Y%m%d").to_i - age.to_i * 10000
@@ -17,9 +9,10 @@ module UsersHelper
     Date.today.strftime("%Y%m%d").to_i - age.to_i * 10000 - 9999
   end
 
-  def calcAge(birthdayStr)
-    if birthdayStr
-      (Date.today.strftime("%Y%m%d").to_i - birthdayStr.to_i) / 10000
+  # 渡された生年月日を年齢に変換して出力
+  def calcAge(birthday_str)
+    if birthday_str
+      (Date.today.strftime("%Y%m%d").to_i - birthday_str.to_i) / 10000
     end
   end
 end
