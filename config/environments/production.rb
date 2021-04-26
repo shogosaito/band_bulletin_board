06d62efdd654201a677717f3c92eaea0c89eb71a
S.Shogo <>
2021-04-27 00:41:13 +0900

    update: 設定ファイルの更新

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'nameless-badlands-54984.herokuapp.com'
+  host = 'band-bulletin-boards.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => "smtp.sendgrid.net",
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,14 +1,4 @@
 if Rails.env.production?
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
-    port: 587,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true,
-  }
 elsif Rails.env.development?
   ActionMailer::Base.delivery_method = :letter_opener
 else
